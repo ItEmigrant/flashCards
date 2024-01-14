2dec49d214e88731ac0c76cbc28d9ad6d7b61341
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-01-14 14:26:32 +0100

    add polymorphic button start

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
 import * as path from 'path'
+
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  test: {
+    environment: 'jsdom',
   },
 })
