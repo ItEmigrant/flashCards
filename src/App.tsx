77530c8e115f5578ddc8c6c2745ef7caec8cc1ec
Bohdan Solomchak <bohdan.jur@gmail.com>
2024-01-26 12:24:33 +0100

    homeWorkStart

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Button } from '@/components/ui/button'
 export function App() {
   return (
     <div>
-      <Button as={'a'} href={'https://google.com'}>
-        hello{' '}
+      <Button as={'a'} fullWidth href={'https://google.com'} variant={'primary'}>
+        Google
       </Button>
     </div>
   )
